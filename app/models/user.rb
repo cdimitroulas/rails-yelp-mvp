ca907ead60a149e5a9ba4df62a5112aee8e503e0
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-09 18:15:49 +0000

    added validations to the User model, including uniqueness on e-mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :restaurants
 
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :email, presence: true
-  # validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 
   has_secure_password
 end
